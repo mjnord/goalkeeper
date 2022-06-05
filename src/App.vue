386885089e87a21302583f0be43e66bfcc0d113d
Jakob Andersson <erikjakobandersson@gmail.com>
2022-06-05 16:51:28 +0200

    Fix folder name, add Header (WIP)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <Toast position="top-right" />
   <div class="flex w-screen">
+    <Header></Header>
+  </div>
+  <div class="flex w-screen h-full">
     <Sidebar></Sidebar>
     <div class="p-4 w-full">
       <router-view></router-view>
@@ -10,5 +13,6 @@
 
 <script setup lang="ts">
 import Toast from "primevue/toast";
-import Sidebar from "./toolbar/Sidebar.vue";
+import Sidebar from "./sidebar/Sidebar.vue";
+import Header from "./header/Header.vue";
 </script>
---
+++ src/header/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex w-screen">
+    <div
+      class="p-3 bg-slate-300 rounded-md shadow-md cursor-pointer m-2 h-16"
+    >
+      <p>
+        {{ t("headerTitle") }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useI18n } from "vue-i18n";
+import { messages } from "./i18n/messages";
+const { t } = useI18n({ messages });
+</script>
