f51520703873652059f0bfb86a0bcc98d85ca0f1
Max Netterberg <max.netterberg@gmail.com>
2022-06-05 21:21:12 +0200

    Added dots button

--- src/pages/nodes-page/components/ListNodeItem.vue
+++ src/pages/nodes-page/components/ListNodeItem.vue
@@ -2,7 +2,7 @@
   <div
     class="flex rounded-md p-2 cursor-pointer hover:shadow-sm hover:bg-white transition-colors"
   >
-    <div class="flex flex-col">
+    <div class="flex flex-col w-40">
       <h1 class="text-lg">{{ title }}</h1>
       <small>{{ url }}</small>
       <ServerNetwork
@@ -19,11 +19,16 @@
       <p>Uptime: {{ format(new Date(), "yyyy-MM-dd") }}</p>
       <p>Added: {{ format(new Date(added), "yyyy-MM-dd") }}</p>
     </div>
+    <Button
+      icon="pi pi-ellipsis-v"
+      class="p-button-rounded p-button-info p-button-text p-button-lg !text-gk-oxford-blue ml-auto my-auto"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
 import Divider from "primevue/divider";
+import Button from "primevue/button";
 import ServerNetwork from "@/assets/icons/ServerNetwork.vue";
 import { format } from "date-fns";
 
